b57cb74d5519737b5b8f4258373de8f0fa4915d4
Berat Cankar <berat.cankar@udemy.com>
2022-02-22 17:23:38 +0300

    Update lib version to make it have constraint

--- setup.py
+++ setup.py
@@ -2,20 +2,32 @@ from os import path
 from setuptools import setup, find_packages
 import sys
 
-sys.path.insert(0, 'src')
+sys.path.insert(0, "src")
 
-with open(path.join('.', 'README.md')) as f:
+with open(path.join(".", "README.md")) as f:
     long_description = f.read()
 
-setup(name='Peniot',
-      version='1.0',
-      description='Penetration Testing Tool for IoT devices',
-      long_description=long_description,
-      author='Berat Cankar,Bilgehan Bingol,Ebru Celebi,Dogukan Cavdaroglu',
-      url='https://senior.ceng.metu.edu.tr/2019/peniot/',
-      platform='Unix',
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=['paho-mqtt', 'scapy', 'pyshark-legacy', 'coapthon', 'Cython', 'pygame', 'pika', 'fpdf', 'pexpect', 'enum'],
-      classifiers=['Programming Language :: Python :: 2.7.9']
+setup(
+    name="Peniot",
+    version="1.0",
+    description="Penetration Testing Tool for IoT devices",
+    long_description=long_description,
+    author="Berat Cankar,Bilgehan Bingol,Ebru Celebi,Dogukan Cavdaroglu",
+    url="https://senior.ceng.metu.edu.tr/2019/peniot/",
+    platform="Unix",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Cython",
+        "paho-mqtt",
+        "scapy",
+        "pyshark-legacy",
+        "coapthon",
+        "fpdf",
+        "pygame==1.9.4",
+        "pika",
+        "pexpect",
+        "enum",
+    ],
+    classifiers=["Programming Language :: Python :: 2.7.9"],
 )
